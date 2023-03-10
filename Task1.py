88af8e3bd008adc4152b07b744432f0a8abce79e
artem <artemkolesnikov@local>
2023-03-10 14:37:38 +0200

    task 1

--- Task1.py
+++ Task1.py
@@ -1,83 +1,28 @@
-import sys
+def get_pharmacies():
+    return (
+        ("Аптека №1", "вул. Шевченка 10", "(044)123-45-67", 100000, 5000),
+        ("Аптека №2", "вул. Пушкіна 20", "(044)234-56-78", 150000, 6000),
+        ("Аптека №3", "вул. Лесі Українки 30", "(044)345-67-89", 80000, 4500),
+        ("Аптека №4", "вул. Тарасівська 40", "(044)456-78-90", 120000, 5500),
+        ("Аптека №5", "вул. Київська 50", "(044)567-89-01", 95000, 4800),
+        ("Аптека №6", "вул. Гоголя 60", "(044)678-90-12", 110000, 5200),
+        ("Аптека №7", "вул. Хрещатик 70", "(044)789-01-23", 135000, 1900)
+    )
 
 
-def read_input_to_list():
-    random_list = []
-    for i in range(0, 12):
-        value = int(input())
-        if value < 0 or value > 200:
-            print("Invalid input. Value should be within 0 to 200")
-            break
-        random_list.append(value)
-    return random_list
+def pharmacies(tuples: tuple):
+    avg_turnover = sum(value[3] for value in tuples) / len(tuples)
+    avg_drugs = sum(value[4] for value in tuples) / len(tuples)
+    biggest_pharmacies = [t for t in tuples if t[3] > avg_turnover and t[4] > avg_drugs]
 
-
-def find_index_identical_values(values: list):
-    master_counter = 0
-    master_index = -1
-
-    counter = 0
-    index = -1
-    for i, item in enumerate(values):
-        if i == 0:
-            continue
-        if values[i - 1] == item:
-            if index == -1:
-                index = i - 1
-            counter += 1
-        else:
-            if master_counter < counter:
-                master_counter = counter
-                counter = 0
-                master_index = index
-                index = -1
-
-    if counter > master_counter:
-        master_index = index
-    print("Start index of same numbers: " + str(master_index))
-
-
-def sum_of_pair_elements(values: list):
-    copy_list = values.copy()
-    sum_of_pair_numbers = 0
-
-    for i, val in enumerate(copy_list):
-        for j, value in enumerate(copy_list[i + 1:], start=i + 1):
-            if value == -1:
-                continue
-            if val == value:
-                sum_of_pair_numbers += val * 2
-                copy_list[i] = -1
-                copy_list[j] = -1
-    print("Sum of pair elements: " + str(sum_of_pair_numbers))
-
-
-def multiply(values: list):
-    result = 1
-    lowest = sys.maxsize
-    lowest_index = -1
-
-    highest = -1
-    highest_index = -1
-
-    for i, item in enumerate(values):
-        if lowest > item:
-            lowest = item
-            lowest_index = i
-        if highest < item:
-            highest = item
-            highest_index = i
-
-    start_index = lowest_index if lowest_index < highest_index else highest_index
-    end_index = lowest_index if lowest_index > highest_index else highest_index
-    for i, item in enumerate(values[start_index:end_index]):
-        result *= item
-    print("Multiply numbers between lowest and highest numbers: " + str(result))
+    print("Середній товарообіг: " + str(avg_turnover))
+    print("Cередня кількість препаратів: " + str(avg_drugs))
+    print()
+    print("Найбільші аптеки:")
+    for item in biggest_pharmacies:
+        print(str(item))
 
 
 if __name__ == '__main__':
-    numbers = read_input_to_list()
-    print(numbers)
-    find_index_identical_values(numbers)
-    sum_of_pair_elements(numbers)
-    multiply(numbers)
+    rows = get_pharmacies()
+    pharmacies(rows)
